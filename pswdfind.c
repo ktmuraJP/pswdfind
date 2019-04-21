83592cc3f577f5d1d07115e5f73047b1a6a66a2c
Keita Murakami <36421890+ktmuraJP@users.noreply.github.com>
2019-04-21 00:02:45 -0400

    Add files via upload

--- pswdfind.c
+++ pswdfind.c
@@ -65,16 +65,11 @@ table_t strsplit(char *str,char *tgt)
     t.str = rstr;
     return t;
 }
-/*
-char *fstrcat(char *dest,char *src)
-{
-    while (*dest) dest++;
-    while (*dest++ = *src++);
-    return --dest;
-}
-*/
-char **dictstrcat(char **ref, char **dict, char **res,  
-        int reflen, int dictlen, int reslen, int depth, int max)
+
+
+
+char **dictstrcat(char **ref, char **dict,  
+        int reflen, int dictlen, int depth, int max)
 {
     int rlen,dlen,catlen;
     int match;
@@ -93,64 +88,45 @@ char **dictstrcat(char **ref, char **dict, char **res,
         return NULL;
     }
 
-    if(res == NULL){
-        res = (char**)malloc(buffsize);
-    }
-
     for(i = 0; i < reflen; ++i){
         rlen = strlen(ref[i]);
 
         for(j = 0; j < dictlen; ++j){
         
-            //fprintf(stderr,"length %d/%d : unaccepted %d/%d ",match,reflen,unmatch,reflen);
             //fprintf(stderr,"  \"\e[93m%s\e[0m\" << \"\e[93m%s\e[0m\" ",ref[i],dict[j]);
         
             dlen = strlen(dict[j]);
             catlen = rlen + dlen;
             
-            if(catlen < max){
+            if(catlen <= max){
 
                 buff[match] = (char*)malloc(sizeof(char)*(catlen));
                 strcpy(buff[match],ref[i]);
                 strcat(buff[match],dict[j]);
-                //fprintf(stderr,"match   \e[92m%s\e[0m <-- (%ld/%d)\n",buff[match],strlen(buff[match]),max);
-                //fprintf(stderr,"\r\e[92m%s\e[0m <-- (%ld/%d)",buff[match],strlen(buff[match]),max);
-
+                //fprintf(stderr,"\runder max   \e[92m%s\e[0m <-- (%ld/%d)",buff[match],strlen(buff[match]),max);
                 ++match;
                 buff = (char**)realloc(buff,sizeof(char*)*(match+1)); 
-
-            }else if(catlen == max){
-
-                res[reslen] = (char*)malloc(sizeof(char)*max);
-                strcpy(res[reslen],ref[i]);
-                strcat(res[reslen],dict[j]);
-
-                //fprintf(stderr,"\rresult   \e[92m%s (%dth)/(%ldmem)\e[0m",res[reslen],reslen,buffsize);
-                //fprintf(stderr,"\rmatched   \e[92m%s%s\e[0m  in %d / %ld",ref[i],dict[j],reslen,buffsize);
-                ++reslen;
-                res = (char**)realloc(res,sizeof(char*)*(reslen+1)); 
-            
             }
             ++loop;
         }  
     }
-    
+
     if(match == 0){
-        res[reslen] = "\0";
         fprintf(stderr,"--==\e[96m FINISHED \e[0m==--\n");
         fprintf(stderr,"loops - \e[95m%d\e[0m\n",loop);
         fprintf(stderr,"depth - \e[95m%d\e[0m\n",depth); 
-        return res;
-    }else{
 
+        return NULL;
+    }else{
         fprintf(stderr,"(depth)\e[92m %d \e[0m",depth);
-        fprintf(stderr,": (match)\e[93m %d \e[0m",match);
-        fprintf(stderr,": (result)\e[93m %d \e[0m\n",reslen);
+        fprintf(stderr,": (match)\e[93m %d \e[0m\n",match);
+
     }
+
     ++depth;
     buff = dictstrcat(
-            buff,dict,res,
-            match,dictlen,reslen,
+            buff,dict,
+            match,dictlen,
             depth,
             max);
 }
@@ -180,25 +156,16 @@ void getargs(int argc, char **argv)
     t = strsplit(fstr,"\n");
     dict = t.str;
     ref = t.str;  
-    res = NULL; 
     len = t.indx;
 
     dcatstr = dictstrcat(
             ref,
             dict,
-            res,
             len,
             len,
-            0,
             0,
             max);
-
     
-    while(strcmp(*dcatstr,"\0")!=0){
-        //fprintf(stderr,"\e[41m%s\e[0m\n",*dcatstr);
-        //fprintf(stdout,"%s\n",*dcatstr);
-        **dcatstr++;
-    }    
     fprintf(stderr,"\n");
 }
 
